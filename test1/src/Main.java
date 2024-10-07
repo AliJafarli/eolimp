d37da290b8227e853c6416440c05fe53e316107f
AliJafarli <somagni4icent@gmail.com>
2024-10-07 19:05:41 +0400

    Solved first task2 changes

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write number: ");
+        int n = sc.nextInt();
+        if (n == 7) {
+            System.out.println("yes");
+        } else if (n == 8) {
+            System.out.println("no");
+        } else {
+            System.out.println("wrong number ");
+        }
     }
 }
--- test2/src/Main.java
+++ test2/src/Main.java
@@ -1,5 +1,107 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!2");
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Write number: ");
+        int n = sc.nextInt();
+
+        if (n < 0 && n > 99) {
+            System.out.println("Incorrect number ");
+        }
+        switch (n) {
+            case 1:
+                System.out.println("bir");
+                break;
+            case 2:
+                System.out.println("iki");
+                break;
+            case 3:
+                System.out.println("üç");
+                break;
+            case 4:
+                System.out.println("dörd");
+                break;
+            case 5:
+                System.out.println("beş");
+                break;
+            case 6:
+                System.out.println("altı");
+                break;
+            case 7:
+                System.out.println("yeddi");
+                break;
+            case 8:
+                System.out.println("səkkiz");
+                break;
+            case 9:
+                System.out.println("doqquz");
+                break;
+
+        }
+        switch (n / 10) {
+            case 1:
+                System.out.println("on");
+                break;
+            case 2:
+                System.out.println("iyirmi");
+                break;
+            case 3:
+                System.out.println("otuz");
+                break;
+            case 4:
+                System.out.println("qırx");
+                break;
+            case 5:
+                System.out.println("əlli");
+                break;
+            case 6:
+                System.out.println("altmış");
+                break;
+            case 7:
+                System.out.println("yetmiş");
+                break;
+            case 8:
+                System.out.println("səksən");
+                break;
+            case 9:
+                System.out.println("doxsan");
+                break;
+        }
+
+        switch (n % 10) {
+            case 1:
+                System.out.println("bir");
+                break;
+            case 2:
+                System.out.println("iki");
+                break;
+            case 3:
+                System.out.println("üç");
+                break;
+            case 4:
+                System.out.println("dörd");
+                break;
+            case 5:
+                System.out.println("beş");
+                break;
+            case 6:
+                System.out.println("altı");
+                break;
+            case 7:
+                System.out.println("yeddi");
+                break;
+            case 8:
+                System.out.println("səkkiz");
+                break;
+            case 9:
+                System.out.println("doqquz");
+                break;
+
+        }
+
     }
 }
